75aad49d07a26fefb9ea5e267cab4d754ed8bb45
Your Name <your.email@example.com>
2026-05-03 19:27:17 +0800

    fix: rate limit issues and missing value in fe

--- Backend/src/middleware/rateLimiters.js
+++ Backend/src/middleware/rateLimiters.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const authRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  limit: 20,
+  windowMs: 60 * 1000,
+  limit: 100,
   standardHeaders: "draft-8",
   legacyHeaders: false
 });
